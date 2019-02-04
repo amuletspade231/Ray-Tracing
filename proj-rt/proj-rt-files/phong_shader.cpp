05be853e7edfdb15e9db2e087b3094b1ba667e9a
Amanda Cao <acao004@bolt.cs.ucr.edu>
2019-02-03 20:50:25 -0800

    added diffuse and specular. needs fixing.

--- proj-rt/proj-rt-files/phong_shader.cpp
+++ proj-rt/proj-rt-files/phong_shader.cpp
@@ -11,9 +11,21 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     vec3 color;
     TODO; //determine the color
     vec3 ambient = world.ambient_color * world.ambient_intensity * color_ambient;
-//    for (Light* light: lights) {
-	
-//    {
-    color = ambient;
+    vec3 diffuse = {0,0,0};
+    vec3 specular = {0,0,0};
+    for (Light* light : world.lights) {
+	vec3 light_vec = light->position - intersection_point;
+	Ray shadow_ray(intersection_point, light_vec.normalized());
+	Hit shadow_hit = world.Closest_Intersection(shadow_ray);
+
+	if (shadow_hit.object == NULL || shadow_hit.dist > light_vec.magnitude()) {
+	    diffuse += color_diffuse * light->Emitted_Light(light_vec) * std::max(dot(normal, shadow_ray.direction), 0.0);
+	    
+	    vec3 reflection = (ray.direction - 2 * dot(ray.direction, normal) * normal).normalized();
+	    vec3 view = - ray.direction;
+	    specular += color_specular * light->Emitted_Light(light_vec) * pow(std::max(dot(view, reflection) ,0.0), specular_power); 
+	}
+    }
+    color = ambient + diffuse + specular;
     return color;
 }
